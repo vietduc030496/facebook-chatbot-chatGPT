75be6488fde1b11ee98cf9aba474dbf50abc2a0c
duc.nguyenviet <nguyenvietduc030496@gmail.com>
2023-03-29 15:33:28 +0700

    add test api

---
+++ src/main/java/com/vti/demo/chatbot/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.vti.demo.chatbot.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+public class TestController {
+
+	
+	@GetMapping("/test")
+	public String test() {
+		return "hello world.";
+	}
+}
